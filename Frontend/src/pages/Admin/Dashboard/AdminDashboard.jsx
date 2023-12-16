2abd591b6920db8f671162d83ded728800fa2d55
bgtti <bru.mgtti@gmail.com>
2023-12-16 12:30:11 +0100

    FE:admin dashboard, admin settings, and modalChangeAccount implementation

--- Frontend/src/pages/Admin/Dashboard/AdminDashboard.jsx
+++ Frontend/src/pages/Admin/Dashboard/AdminDashboard.jsx
@@ -5,8 +5,33 @@ function AdminDashboard() {
 
     return (
         <div className="AdminDashboard">
-            <h2>Admin Dashboard</h2>
-            <p>hello</p>
+            <h3>Admin Dashboard</h3>
+            <section className="AdminDashboard-Section1">
+                <div>
+                    <p>10</p>
+                    <p>registered users</p>
+                </div>
+                <div>
+                    <p>10</p>
+                    <p>website visitors</p>
+                </div>
+            </section>
+            <section className="AdminDashboard-Section2">
+                <div>
+                    <div className="AdminDashboard-Suspiscious"></div>
+                    <p>2 logs marked 'suspiscious'</p>
+                    <div>
+                        <button>view</button>
+                    </div>
+                </div>
+                <div>
+                    <div className="AdminDashboard-Warn"></div>
+                    <p>3 logs marked 'warn'</p>
+                    <div>
+                        <button>view</button>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
--- Frontend/src/pages/Admin/Settings/AdminSettings.jsx
+++ Frontend/src/pages/Admin/Settings/AdminSettings.jsx
@@ -1,10 +1,50 @@
+import { useState } from "react";
+import Modal from "../../../components/Modal/Modal";
+import ModalChangeAccount from "./ModalChangeAccount";
+import "./adminSettings.css"
 
+const ACCOUNT_ACTIONS = ["password", "email"]
 function AdminSettings() {
+    const [modalChangeAccount, setModalChangeAccount] = useState(false)
+    const [accountAction, setAccountAction] = useState("")
+
+    modalChangeAccount ? document.body.classList.add("Modal-active") : document.body.classList.remove("Modal-active");
+
+    const modalChangeAccountContent = modalChangeAccount && accountAction !== "" && (
+        <ModalChangeAccount action={accountAction} modalToggler={toggleModal} />
+    );
+
+    function selectAccountAction(action) {
+        ACCOUNT_ACTIONS.includes(action.toLowerCase()) ? setAccountAction(action) : setAccountAction("");
+    }
+    function toggleModal() {
+        setModalChangeAccount(!modalChangeAccount);
+    }
 
     return (
         <div className="AdminSettings">
-            <h2>Settings</h2>
-            <p>settings....</p>
+            {
+                modalChangeAccount && accountAction !== "" && (
+                    <Modal
+                        title={`Change admin ${accountAction}`}
+                        content={modalChangeAccountContent}
+                        modalStatus={modalChangeAccount}
+                        setModalStatus={setModalChangeAccount} ></Modal>
+                )
+            }
+            <h3>Admin Settings</h3>
+            <section className="AdminSettings-Section1">
+                <h4>Account</h4>
+                <p>Email: admin@admin.com</p>
+                <div>
+                    <button onClick={() => { selectAccountAction("email"); toggleModal() }}>
+                        Change email
+                    </button>
+                    <button onClick={() => { selectAccountAction("password"); toggleModal() }}>
+                        Change password
+                    </button>
+                </div>
+            </section>
         </div>
     );
 }
